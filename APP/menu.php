6e8dd1e06c3b2addcaad8f64822f68f0dee21526
Diego <diego@diego.com>
2019-08-09 13:08:34 +0200

    Subir APP

--- APP/menu.php
+++ APP/menu.php
@@ -13,11 +13,11 @@
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand " href="#"><img src="images/Logo/logo_tripAddict_blanco.png" style="width:70px"/></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-
+  
   <div class="collapse navbar-collapse" id="navbarColor01">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
@@ -26,23 +26,48 @@
       <li class="nav-item">
         <a class="nav-link" href="#">Viaje Sorpresa</a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contacto</a>
+      </li>
       
     </ul>
+
     <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
         <a class="nav-link" href="#"><i class="fa fa-shopping-cart fa-lg" aria-hidden="true"></i></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#"><i class="fa fa-user fa-lg" aria-hidden="true"></i></a>
+
+      
+      <!--inicio DROPDOWN-->
+      <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
+     aria-haspopup="true" aria-expanded="false"><i class="fa fa-user fa-lg" aria-hidden="true"> </i> Usuario</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Mi cuenta</a>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="#">Mis packs</a>
+      
+    </div>
       </li>
+      
+      <!--final DROPDOWN-->
+
       <li class="nav-item">
         <a class="nav-link" href="#"><i class="fa fa-power-off fa-lg" aria-hidden="true"></i></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Iniciar sesión</a>
       </li>
       
     </ul>
     
   </div>
 </nav>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </body>
 </html>

d740f1de3d008bcd05e7ca9ba76c8c3b1dbb83ce
Diego <diego@diego.com>
2019-08-09 13:20:05 +0200

    subir nav

--- APP/nav.php
+++ APP/nav.php
@@ -1,9 +1,11 @@
+
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand " href="#"><img src="images/Logo/logo_tripAddict_blanco.png" style="width:70px"/></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-
+  
   <div class="collapse navbar-collapse" id="navbarColor01">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
@@ -12,17 +14,37 @@
       <li class="nav-item">
         <a class="nav-link" href="#">Viaje Sorpresa</a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contacto</a>
+      </li>
       
     </ul>
+
     <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
         <a class="nav-link" href="#"><i class="fa fa-shopping-cart fa-lg" aria-hidden="true"></i></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#"><i class="fa fa-user fa-lg" aria-hidden="true"></i></a>
+
+      
+      <!--inicio DROPDOWN-->
+      <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
+     aria-haspopup="true" aria-expanded="false"><i class="fa fa-user fa-lg" aria-hidden="true"> </i> Usuario</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Mi cuenta</a>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="#">Mis packs</a>
+      
+    </div>
       </li>
+      
+      <!--final DROPDOWN-->
+
       <li class="nav-item">
         <a class="nav-link" href="#"><i class="fa fa-power-off fa-lg" aria-hidden="true"></i></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Iniciar sesión</a>
       </li>
       
     </ul>
